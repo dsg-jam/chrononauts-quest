ff76a0dcb7c2eacea6d55cd4f312b3a303aa63a1
Simon Berger <simon@siku2.io>
2024-08-18 22:43:58 +0000

    devcontainer: fix gcloud install

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+_apt_install=(apt-get -o 'DPkg::Lock::Timeout=120' install -y)
+
 install_rust() {
     # install dependencies for building espflash from source.
     # at the time of writing the quickinstall binstall method is broken.
-    apt-get -o DPkg::Lock::Timeout=120 install -y \
+    "${_apt_install[@]}" \
         libudev-dev \
         pkg-config
 
     # also required for esp
-    apt-get -o DPkg::Lock::Timeout=120 install -y \
+    "${_apt_install[@]}" \
         clang \
         libclang-dev
 
     # specific deps copied from <https://docs.espressif.com/projects/esp-idf/en/latest/esp32/get-started/linux-macos-setup.html#for-linux-users>
-    apt-get -o DPkg::Lock::Timeout=120 install -y \
+    "${_apt_install[@]}" \
         git wget flex bison gperf python3 python3-pip python3-venv cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0
 
     # install rustup with nightly toolchain and rust-src
@@ -32,7 +34,7 @@ install_rust() {
 }
 
 install_jekyll() {
-    apt-get -o DPkg::Lock::Timeout=120 install -y \
+    "${_apt_install[@]}" \
         ruby \
         ruby-dev \
         ruby-rubygems
@@ -43,34 +45,45 @@ install_jekyll() {
 }
 
 install_gcloud() {
-    local installer_path="/tmp/install-gcloud.sh"
+    "${_apt_install[@]}" \
+        python3
 
+    local installer_path="/tmp/install-gcloud.sh"
     curl https://sdk.cloud.google.com >"$installer_path"
     bash "$installer_path" --disable-prompts
-    bash /root/google-cloud-sdk/install.sh --path-update=true
+    bash /root/google-cloud-sdk/install.sh --quiet --path-update=true
 
     rm -f "$installer_path"
 }
 
 install_node() {
-    apt-get -o DPkg::Lock::Timeout=120 install -y \
+    "${_apt_install[@]}" \
         nodejs \
         npm
 }
 
 main() {
-    set -x
-
     export DEBIAN_FRONTEND=noninteractive
-
     apt-get update
 
-    install_jekyll &
-    install_rust &
-    install_gcloud &
-    install_node &
+    set -x
 
-    wait
+    install_jekyll &
+    local jekyll_pid=$!
+    install_rust &
+    local rust_pid=$!
+    install_gcloud &
+    local gcloud_pid=$!
+    install_node &
+    local node_pid=$!
+
+    for name in "jekyll" "rust" "gcloud" "node"; do
+        local pid_name pid
+        pid_name="${name}_pid"
+        pid="${!pid_name}"
+        echo "Waiting for $name"
+        wait "$pid"
+    done
 
     rm -rf "$0"
 }
