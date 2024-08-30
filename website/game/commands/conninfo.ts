a62728880bee03b7d5e92294912a332898595ebe
Simon Berger <simon@siku2.io>
2024-08-30 20:06:41 +0000

    make the conninfo more consistent

--- website/game/commands/conninfo.ts
+++ website/game/commands/conninfo.ts
@@ -9,13 +9,20 @@ export default {
       // players haven't connected the board to wifi yet
       case "L0":
       case "L1":
-        await terminal.type("No communication device detected.");
+        await terminal.type([
+          "D1: missing",
+          "D2: missing",
+          "SYNC: N/A",
+          "",
+          "No communication device detected.",
+        ]);
         break;
       // players haven't synched the board frequency yet
       case "L2":
-        await terminal.type("D1: detected");
-        await terminal.type("D2: missing");
         await terminal.type([
+          "D1: detected",
+          "D2: missing",
+          "SYNC: N/A",
           "",
           "Frequency mismatch.",
           "Communication corrupted.",
@@ -23,20 +30,23 @@ export default {
         break;
       // players haven't decoded encryption key yet
       case "L3":
-        await terminal.type("D1: detected");
-        await terminal.type("D2: detected");
-        await terminal.type("SYNC: corrupted");
         await terminal.type([
+          "D1: detected",
+          "D2: detected",
+          "SYNC: corrupted",
           "",
           "Sync payload mismatch.",
           "Encryption code might be invalid. Use 'decrypt' command to update it.",
         ]);
         break;
       default:
-        await terminal.type("D1: detected");
-        await terminal.type("D2: detected");
-        await terminal.type("SYNC: complete");
-        await terminal.type(["", "Connection established."]);
+        await terminal.type([
+          "D1: detected",
+          "D2: detected",
+          "SYNC: complete",
+          "",
+          "Connection established.",
+        ]);
     }
   },
 } as Command;
